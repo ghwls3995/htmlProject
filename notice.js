8304e29e351cb7d4ecc88ee152745562b8ad81d3
NaHoJin <ghwls3995@gmail.com>
2024-04-18 10:46:48 +0900

    04.18 1차수정

--- notice.js
+++ notice.js
@@ -5,3 +5,20 @@ function changeImage(imageUrl) {
 function purchase() {
     alert("죄송합니다. 해당 제품은 현재 품절되었습니다.");
 }
+
+
+function preparing() {
+    alert("죄송합니다. 해당 제품은 현재 준비중입니다.");
+}
+
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username.trim() === "" || password.trim() === "") {
+        alert("아이디와 비밀번호를 모두 입력해주세요.");
+        return false;
+    }
+
+    return true;
+}
